d585037ae4419eb187358389f305c45ebbc87115
Stefan Petrovski <stefan.petrovski@gmail.com>
2020-12-16 08:21:09 +0100

    day 15

---
+++ day15/1.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+# test
+# numbers = [2,3,1]
+
+# input
+numbers = [2,15,0,9,1,20]
+
+spoken = defaultdict(list)
+
+for i, n in enumerate(numbers):
+    spoken[n].append(i)
+
+while len(numbers) != 2020:
+    v = numbers[-1]
+    l = spoken[v]
+    if len(l) <= 1:
+        numbers.append(0)
+    else:
+        diference = spoken[v][-1] - spoken[v][-2]
+        numbers.append(diference)
+    spoken[numbers[-1]].append(len(numbers) - 1)
+
+print(numbers[-1])
